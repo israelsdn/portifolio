545b93b7c1019aa42d27ddcde3264f0af68e318d
Israel Sales Dantas do Nascimento <israelsales043@gmail.com>
2023-04-16 12:02:27 -0300

    teste-tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div 
+      className='h-screen bg-[#09090B]'>
+        <main 
+        className='h-screen flex justify-center items-center'>
+          <div 
+          className='grid bg-white rounded-3xl'>
+            <h1 
+            className='max-sm:text-xl max-lg:text-2xl text-3xl font-bold text-center text-gray-800 mt-8 mb-10'>
+              LOGIN
+            </h1>
+
+            <input  
+            placeholder='User' 
+            type="text"
+            className='text-input-login mx-16 mb-5'/>
+
+            <input  
+            placeholder='Password' 
+            type="password"
+            className='text-input-login mx-16 mb-12 '/>
+            
+            <button 
+            className='max-sm:text-sm max-lg:text-base text-lg bg-purple-900 hover:bg-purple-700 text-[#09090B] font-bold py-2 px-4 rounded
+             mx-5 mb-12'>
+              ENTER
+            </button>
+
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
-
-export default App;
