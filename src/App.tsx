eb04c79f8dd30b7959565b2f8d2344183dde72d2
Israel Sales Dantas do Nascimento <israelsales043@gmail.com>
2023-04-18 11:37:42 -0300

    Header add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './App.css';
 import { Main } from './components/Main';
+import { Header } from './components/Header';
 
 export function App() {
   return (
     <>
-      <Main/>
+      <header>
+        <Header/>
+      </header>
+      <main>
+        <Main/>
+      </main>
+      <footer></footer>
     </>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+export function Header(){
+    return (
+        <div className="text-white bg-purple-700 h-10 flex items-center">
+                <button className="absolute right-8 text-sm font-semibold">LOGIN</button>
+                <button className="absolute right-24 text-sm font-semibold">REGISTER</button>
+        </div>
+    )
+}
